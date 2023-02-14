1dba2d89fe7c19d25fbc0ed2d84235aa425d1338
aimproxy <8200383@estg.ipp.pt>
2023-02-14 14:41:26 +0000

    Add Elapsed Time Updater

--- app/src/main/java/com/aimproxy/chargify/components/TimelineListItem.kt
+++ app/src/main/java/com/aimproxy/chargify/components/TimelineListItem.kt
@@ -1,12 +1,13 @@
 package com.aimproxy.chargify.components
 
-import android.util.Log
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.History
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.text.font.FontWeight
 import com.aimproxy.chargify.firestore.TimelinesAggregation.*
+import com.google.firebase.Timestamp
+import kotlinx.coroutines.delay
 import java.util.*
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -14,7 +15,7 @@ import java.util.*
 fun TimelineListItem(
     lastKnownEvStation: LastKnownEvStation,
 ) {
-    Log.d("Timeline", lastKnownEvStation.stationId.toString())
+
     ListItem(
         headlineText = {
             lastKnownEvStation.addressInfo?.let {
@@ -23,19 +24,7 @@ fun TimelineListItem(
         },
         supportingText = {
             lastKnownEvStation.timestamp?.let {
-                val elapsedTimeMs = Date().time - it.toDate().time
-                val elapsedSeconds = (elapsedTimeMs / 1000).toInt()
-                val elapsedMinutes = (elapsedSeconds / 60)
-                val elapsedHours = (elapsedMinutes / 60)
-                val elapsedDays = (elapsedHours / 24)
-
-                val elapsedStr = when {
-                    elapsedDays > 0 -> "$elapsedDays days ago"
-                    elapsedHours > 0 -> "$elapsedHours hours ago"
-                    elapsedMinutes > 0 -> "$elapsedMinutes minutes ago"
-                    else -> "$elapsedSeconds seconds ago"
-                }
-                Text(elapsedStr, fontWeight = FontWeight.Bold)
+                TimelineTicker(it)
             }
         },
         leadingContent = {
@@ -44,4 +33,31 @@ fun TimelineListItem(
             }
         },
     )
+}
+
+@Composable
+fun TimelineTicker(
+    timestamp: Timestamp
+) {
+    var elapsedStr by remember { mutableStateOf("") }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            val elapsedTimeMs = Date().time - timestamp.toDate().time
+            val elapsedSeconds = (elapsedTimeMs / 1000).toInt()
+            val elapsedMinutes = (elapsedSeconds / 60)
+            val elapsedHours = (elapsedMinutes / 60)
+            val elapsedDays = (elapsedHours / 24)
+
+            elapsedStr = when {
+                elapsedDays > 0 -> "$elapsedDays days ago"
+                elapsedHours > 0 -> "$elapsedHours hours ago"
+                elapsedMinutes > 0 -> "$elapsedMinutes minutes ago"
+                else -> "$elapsedSeconds seconds ago"
+            }
+            delay(1000L)
+        }
+    }
+
+    Text(elapsedStr, fontWeight = FontWeight.Bold)
 }
